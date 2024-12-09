0e25ff7226dd82f95a33e00947ce5b8791d8ba03
vizvasrj <vizvasrj@gmail.com>
2024-12-10 00:41:26 +0530

    cli flags added

--- client/main.go
+++ client/main.go
@@ -4,96 +4,145 @@ import (
 	"context"
 	"io"
 	"log"
+	"os"
 	"sync"
 	"time"
 
 	pb "src/proto"
 
+	"github.com/urfave/cli/v2"
+
 	cp "github.com/atotto/clipboard"
 	"golang.design/x/clipboard"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	// dail server
-	conn, err := grpc.Dial(":50005", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("can not connect with server %v", err)
-	}
+	app := &cli.App{
+		Name:  "gRPC Clipboard Client",
+		Usage: "A client to send and receive clipboard data via gRPC",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "ip",
+				Usage: "IP address of the gRPC server",
+				Value: "localhost:50005",
+			},
+			&cli.BoolFlag{
+				Name:  "insecure",
+				Usage: "Use insecure gRPC connection",
+				Value: true,
+			},
+			&cli.StringFlag{
+				Name:     "tls",
+				Usage:    "Path to TLS certificate",
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:     "group",
+				Usage:    "Group name",
+				Required: false,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			ip := c.String("ip")
+			insecureConn := c.Bool("insecure")
 
-	// create stream
-	client := pb.NewMathClient(conn)
-	stream, err := client.Max(context.Background())
-	if err != nil {
-		log.Fatalf("openn stream error %v", err)
-	}
-
-	var max string
-	ctx := stream.Context()
-	done := make(chan bool)
-
-	isRecieved := false
-	mutex := &sync.Mutex{}
-	// first goroutine sends random increasing numbers to stream
-	// and closes it after 10 iterations
-	ch := clipboard.Watch(context.TODO(), clipboard.FmtText)
-	go func() {
-		for data := range ch {
-			if !isRecieved {
-				req := pb.Request{String_: string(data)}
-				if err := stream.Send(&req); err != nil {
-					log.Printf("can not send %v\n", err)
-					continue
-				}
-				// log.Printf("%s sent", req.String_)
-				time.Sleep(time.Millisecond * 200)
+			// Set up gRPC connection
+			var opts []grpc.DialOption
+			if insecureConn {
+				opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 			} else {
-				// fmt.Println("isRecieved data that is i am not running send clipboard because i just recieved")
-				mutex.Lock()
-				isRecieved = false
-				mutex.Unlock()
+				// Add your secure credentials here
+				if c.String("tls") == "" {
+					log.Fatalf("TLS certificate required")
+				} else {
+					creds, err := credentials.NewClientTLSFromFile(c.String("tls"), "")
+					if err != nil {
+						log.Fatalf("could not load tls cert: %s", err)
+					}
+					opts = append(opts, grpc.WithTransportCredentials(creds))
+				}
 			}
-		}
-		if err := stream.CloseSend(); err != nil {
-			log.Println(err)
-		}
-	}()
 
-	// second goroutine receives data from stream
-	// and saves result in max variable
-	//
-	// if stream is finished it closes done channel
-	go func() {
-		for {
-			resp, err := stream.Recv()
-			if err == io.EOF {
-				close(done)
-				return
-			}
+			conn, err := grpc.NewClient(ip, opts...)
 			if err != nil {
-				log.Fatalf("can not receive %v", err)
+				log.Fatalf("can not connect with server %v", err)
 			}
-			max = resp.String_
-			// log.Printf("recieved Clipboard [%s]", max)
-			mutex.Lock()
-			isRecieved = true
-			mutex.Unlock()
-			// clipboard.Write(clipboard.FmtText, []byte(max))
-			cp.WriteAll(max)
-		}
-	}()
+			defer conn.Close()
 
-	// third goroutine closes done channel
-	// if context is done
-	go func() {
-		<-ctx.Done()
-		if err := ctx.Err(); err != nil {
-			log.Println(err)
-		}
-		close(done)
-	}()
+			// create stream
+			client := pb.NewMathClient(conn)
+			stream, err := client.Max(context.Background())
+			if err != nil {
+				log.Fatalf("open stream error %v", err)
+			}
 
-	<-done
-	// log.Printf("finished with max=%s", max)
+			var max string
+			ctx := stream.Context()
+			done := make(chan bool)
 
+			isRecieved := false
+			mutex := &sync.Mutex{}
+
+			// first goroutine sends clipboard data to stream
+			ch := clipboard.Watch(context.TODO(), clipboard.FmtText)
+			go func() {
+				for data := range ch {
+					if !isRecieved {
+						req := pb.Request{String_: string(data), Group: c.String("group")}
+						if err := stream.Send(&req); err != nil {
+							log.Printf("can not send %v\n", err)
+							continue
+						}
+						time.Sleep(time.Millisecond * 200)
+					} else {
+						mutex.Lock()
+						isRecieved = false
+						mutex.Unlock()
+					}
+				}
+				if err := stream.CloseSend(); err != nil {
+					log.Println(err)
+				}
+			}()
+
+			// second goroutine receives data from stream
+			go func() {
+				for {
+					resp, err := stream.Recv()
+					if err == io.EOF {
+						close(done)
+						return
+					}
+					if err != nil {
+						log.Fatalf("can not receive %v", err)
+					}
+					max = resp.String_
+					mutex.Lock()
+					isRecieved = true
+					mutex.Unlock()
+					cp.WriteAll(max)
+				}
+			}()
+
+			// third goroutine closes done channel if context is done
+			go func() {
+				<-ctx.Done()
+				if err := ctx.Err(); err != nil {
+					log.Println(err)
+				}
+				close(done)
+			}()
+
+			<-done
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- server/main.go
+++ server/main.go
@@ -6,13 +6,18 @@ import (
 	"io"
 	"log"
 	"net"
-	pb "src/proto"
+	"os"
 	"sync"
 	"time"
+
+	pb "src/proto"
+
+	"github.com/urfave/cli/v2"
 
 	cp "github.com/atotto/clipboard"
 	"golang.design/x/clipboard"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type server struct {
@@ -31,7 +36,6 @@ func (s server) Max(srv pb.Math_MaxServer) error {
 					log.Printf("can not send %v\n", err)
 					continue
 				}
-				// log.Printf("from gnome to lkde [%s] sent", req.String_)
 				time.Sleep(time.Millisecond * 200)
 			} else {
 				mutex.Lock()
@@ -42,23 +46,17 @@ func (s server) Max(srv pb.Math_MaxServer) error {
 	}()
 
 	log.Println("start new server")
-	// var max = "a"
 	ctx := srv.Context()
 
 	for {
-
-		// exit if context is done
-		// or continue
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		default:
 		}
 
-		// receive data from stream
 		req, err := srv.Recv()
 		if err == io.EOF {
-			// return will close stream from server side
 			log.Println("exit")
 			return nil
 		}
@@ -70,44 +68,82 @@ func (s server) Max(srv pb.Math_MaxServer) error {
 		mutex.Lock()
 		isRecieved = true
 		mutex.Unlock()
-
 		err = cp.WriteAll(req.String_)
 		if err != nil {
 			fmt.Println("error in writing to clipboard", err)
 			continue
 		}
-
-		// continue if number reveived from stream
-		// less than max
-		// if req.String_ == max {
-		// 	continue
-		// }
-
-		// // update max and send it to stream
-		// max = req.String_
-		// now := time.Now().String()
-		// resp := pb.Response{String_: max + "CHANGED at" + now}
-		// if err := srv.Send(&resp); err != nil {
-		// 	log.Printf("send error %v", err)
-		// }
-		// log.Printf("send new max=%s", max)
 	}
 }
 
 func main() {
-	// create listener
-	lis, err := net.Listen("tcp", ":50005")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	app := &cli.App{
+		Name:  "gRPC Clipboard Server",
+		Usage: "A server to send and receive clipboard data via gRPC",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "port",
+				Usage:    "Port to run the gRPC server on",
+				Value:    "50005",
+				Required: false,
+			},
+			&cli.BoolFlag{
+				Name:  "insecure",
+				Usage: "Use insecure gRPC connection",
+				Value: true,
+			},
+			&cli.StringFlag{
+				Name:  "cert",
+				Usage: "Path to the TLS certificate file",
+			},
+			&cli.StringFlag{
+				Name:  "key",
+				Usage: "Path to the TLS key file",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			port := c.String("port")
+			insecureConn := c.Bool("insecure")
+			certFile := c.String("cert")
+			keyFile := c.String("key")
+			log.Println(certFile, keyFile)
+
+			// create listener
+			log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+			lis, err := net.Listen("tcp", ":"+port)
+			if err != nil {
+				log.Fatalf("failed to listen: %v", err)
+			}
+
+			// create grpc server
+			var opts []grpc.ServerOption
+			if insecureConn {
+				// opts = append(opts, grpc.Creds(insecure.NewCredentials()))
+			} else {
+				if certFile == "" || keyFile == "" {
+					log.Fatalf("cert and key files must be provided for TLS")
+				}
+				creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+				if err != nil {
+					log.Fatalf("failed to load TLS credentials: %v", err)
+				}
+				opts = append(opts, grpc.Creds(creds))
+			}
+
+			s := grpc.NewServer(opts...)
+			pb.RegisterMathServer(s, &server{})
+
+			// and start...
+			if err := s.Serve(lis); err != nil {
+				log.Fatalf("failed to serve: %v", err)
+			}
+			return nil
+		},
 	}
 
-	// create grpc server
-	s := grpc.NewServer()
-
-	pb.RegisterMathServer(s, &server{})
-
-	// and start...
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
